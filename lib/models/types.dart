bcf47bdf773b8790d786e64a5c5fa74ef4b506c9
Leonardo Ruiz <leoruizz14@outlook.com>
2023-12-04 16:53:42 -0300

    create Types class

---
+++ lib/models/types.dart
@@ -0,0 +1,17 @@
+import 'package:alura_flutter_client_control1/models/client_type.dart';
+import 'package:flutter/material.dart';
+
+class Types extends ChangeNotifier {
+  List<ClientType> types;
+
+  Types({
+    required this.types,
+  });
+
+  void addType({required ClientType type}) {
+    types.add(type);
+    notifyListeners(); //It warns the ChangeNotifier, that reloads the screen
+  }
+
+  // método de remoção.
+}
